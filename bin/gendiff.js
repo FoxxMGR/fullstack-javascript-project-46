49ead786bad7a21694c567bcb3a6ebf1de5ec3f7
FoxxMGR <magomacievgadzimurad@gmail.com>
2025-10-04 09:11:18 +0300

    шаг 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-V, --version', 'output the version number')
-
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
-
